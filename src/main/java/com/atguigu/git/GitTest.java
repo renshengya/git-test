a34b9646be4a5f1b83c5348f1a83aa1bc9588af8
xiaoLi <2368278547@qq.com>
2022-08-08 19:59:13 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-08-08-19:50
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("你好！");
+    }
+}
