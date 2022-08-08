90ff42c24a598cd4ddc351bdaa00b24268713ef8
xiaoLi <2368278547@qq.com>
2022-08-08 20:31:21 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,7 @@ public class GitTest {
         System.out.println("人");
         System.out.println("sss");
         System.out.println("hello git4");
+        System.out.println("hot-fix test");
+
     }
 }
